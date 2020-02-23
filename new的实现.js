a41914c09224946a8cb0892a41c2d8411c110d4e
fx159 <1175267564@qq.com>
2020-02-23 14:03:06 +0800

    new的实现

---
+++ new的实现.js
@@ -0,0 +1,10 @@
+function createnew(){
+    const obj=Object.create(null);//创建一个空对象
+    con=[].shift.call(arguments);//取出arguments的第一个元素，即传入的构造函数
+    const Fun=function(){};//创建一个构建函数
+    Fun.prototype=con.prototype;
+    obj=new Fun();//让obj指向构造函数，这样就能访问传入的构造函数原型的属性
+    con.apply(obj,arguments);//使用apply使obj的this指向构造函数，这样就能访问构造函数的属性
+    return obj;//返回创建的对象
+
+}
